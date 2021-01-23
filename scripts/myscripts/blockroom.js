a0bc39fa286dc869cbafdace20bc259a22802eea
Prof. Tushar Gohil <goheltushar1401@gmail.com>
2021-01-23 11:39:24 +0530

    g

--- scripts/myscripts/blockroom.js
+++ scripts/myscripts/blockroom.js
@@ -1,4 +1,5 @@
 var block_room = [
-    { "1": "F-201" },
-    { "2": "F-202" }
+    { "block" : "1", "Room_No" : "F-201" },
+    { "block" : "2", "Room_No" : "F-202" },
+    { "block" : "3", "Room_No" : "F-203" }
 ]
---
+++ scripts/myscripts/myscript.js
@@ -0,0 +1,39 @@
+let seatno = 'E723064';
+
+function searchBlock(seatNo, myArray) {
+    for (let i = 0; i < myArray.length; i++) {
+        for (let j = 0; j < myArray[i].seat_nos.length; j++) {
+            if (myArray[i].seat_nos[j] === seatNo)
+                return myArray[i].block;
+        }
+    }
+}
+
+
+function searchRoom_No(block, myArray) {
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i].block === block)
+            return myArray[i].Room_No;
+    }
+}
+
+
+function searchLocation(room_no, myArray) {
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i].Room_No === room_no)
+            return myArray[i].Location;
+    }
+}
+
+function check(seatno){
+    //alert(seatno.value);
+    let block = searchBlock(seatno.value, block_seatnumbers);
+    let room_no = searchRoom_No(block, block_room);
+    let room_location = searchLocation(room_no, rooms);
+    alert(block + " : " + room_no + " : " + room_location); 
+}
+
+//checkButton.addEventListener("click", check(document.getElementById('seatNo')));
+
+
+
